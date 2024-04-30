73f2f587372485cd4073cb817386532cd03bca64
Travis <travislogant@gmail.com>
2024-04-30 19:29:26 +0100

    created a starting screen and a sample test

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import curses
 from curses import wrapper
 
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to start the test.")
+    stdscr.refresh()
+    stdscr.getkey()
+    
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app"
+    current_text = []
+    
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
-    stdscr.clear()
-    stdscr.addstr("Hello World", curses.color_pair(1))
-    stdscr.refresh()
-    stdscr.getkey()
+    start_screen(stdscr)
     
 wrapper(main)
