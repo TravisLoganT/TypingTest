4405a637ffe6d9a56d75dd430d758abe8a8cab85
Travis <travislogant@gmail.com>
2024-04-30 19:43:24 +0100

    allow users of different OS to backspace correctly

--- main.py
+++ main.py
@@ -28,7 +28,12 @@ def wpm_test(stdscr):
         if ord(key) == 27:
             break
         
-        current_text.append(key)
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+        
         
         
     
