98436978d5e419c89e0b5181dda5f3c40d4d704d
Travis <travislogant@gmail.com>
2024-04-30 19:49:56 +0100

    removed extra whitespaces

--- main.py
+++ main.py
@@ -35,8 +35,6 @@ def wpm_test(stdscr):
             current_text.append(key)
         
         
-        
-    
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
