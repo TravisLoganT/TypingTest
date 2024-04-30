3679d91a30d932ccc666e5a1e561c0812d8e1756
Travis <travislogant@gmail.com>
2024-04-30 19:52:39 +0100

    removed whitespace issues

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ def start_screen(stdscr):
     stdscr.addstr("\nPress any key to start the test.")
     stdscr.refresh()
     stdscr.getkey()
-    
+
 
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app"
     current_text = []
-    
+
     while True:
         stdscr.clear()
         stdscr.addstr(target_text)
-        
+    
         for char in current_text:
             stdscr.addstr(char, curses.color_pair(1))
-            
+ 
         stdscr.refresh()
-        
+
         key = stdscr.getkey()
-        
+
         if ord(key) == 27:
             break
-        
+
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
@@ -40,8 +40,7 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    
     start_screen(stdscr)
     wpm_test(stdscr)
-    
+
 wrapper(main)
